42757886bd0bb477b23da662281e1f62db376a2c
xiaozhenano <xiaozhedesu0219@outlook.com>
2023-11-21 11:11:27 +0800

    P5729 工艺品制作 全局变量的三维数组就不用考虑函数传参方式了，好耶

---
+++ code-P/P5729.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int cube[20][20][20];
+
+void cutCube(int operate[6]);
+
+int nokoriCubes(int a,int b,int c);
+
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	for(int outer = 0; outer < a; outer++) {
+		for(int middle = 0; middle < b; middle++) {
+			for(int inner = 0; inner < c; inner++) {
+				cube[outer][middle][inner] = 1;
+			}
+		}
+	}
+	int num;
+	scanf("%d",&num);
+	int operation[num][6];
+	for(int outer = 0; outer < num; outer++) {
+		for(int inner = 0; inner < 6; inner++) {
+			scanf("%d",&operation[outer][inner]);
+			operation[outer][inner]--;
+		}
+	}
+
+	for(int outer = 0; outer < num; outer++) {
+		cutCube(operation[outer]);
+	}
+
+	printf("%d",nokoriCubes(a,b,c));
+	return 0;
+}
+
+void cutCube(int operate[6]) {
+	for(int outer = operate[0]; outer <= operate[3]; outer++) {
+		for(int middle = operate[1]; middle <= operate[4]; middle++) {
+			for(int inner = operate[2]; inner <= operate[5]; inner++) {
+				cube[outer][middle][inner] = 0;
+			}
+		}
+	}
+}
+
+int nokoriCubes(int a,int b,int c) {
+	int count = 0;
+	for(int outer = 0; outer < a; outer++) {
+		for(int middle = 0; middle < b; middle++) {
+			for(int inner = 0; inner < c; inner++) {
+				if(cube[outer][middle][inner] == 1) {
+					count++;
+				}
+			}
+		}
+	}
+	return count;
+}
